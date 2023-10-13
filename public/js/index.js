eef5587fcc26e90bb8c0f813cfa77b84d74d91d9
iti-0204 <yasuken0327@icloud.com>
2023-10-13 20:00:44 +0900

    lottie終了後の挙動一部完成

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,12 @@
-console.log("あ");
+// スクロールを禁止にする関数
+function disableScroll(event) {
+  event.preventDefault();
+}
 
 window.onload = function () {
+  //スクロール禁止
+  document.addEventListener("touchmove", disableScroll, { passive: false });
+  document.addEventListener("mousewheel", disableScroll, { passive: false });
   var firstView = document.getElementById("first-view");
 
   const animation = lottie.loadAnimation({
@@ -10,6 +16,20 @@ window.onload = function () {
     autoplay: true,
     path: "https://lottie.host/db2d4ce2-e080-4866-b13c-a53d95e83b58/7KIC8gEqKH.json",
   });
+
+  //アニメーション終了後の処理
+  animation.onComplete = () => {
+    //lottieアニメーションの要素を消す
+    firstView.classList.add("delete");
+
+    // スクロール解除
+    document.removeEventListener("touchmove", disableScroll, {
+      passive: false,
+    });
+    document.removeEventListener("mousewheel", disableScroll, {
+      passive: false,
+    });
+  };
 
   animation.play();
 };
