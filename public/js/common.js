c90997a083a1c233bce95c23f2b459788cdca481
iti-0204 <yasuken0327@icloud.com>
2023-11-02 11:30:57 +0900

    title調整、ハンバーガー調整

--- public/js/common.js
+++ public/js/common.js
@@ -28,8 +28,11 @@
 })(document);
 
 $(function () {
-  $("#js-hamburger-menu, .navigation__link").on("click", function () {
-    $(".navigation").slideToggle(500);
-    $(".hamburger-menu").toggleClass("hamburger-menu--open");
-  });
+  $("#js-hamburger-menu, .navigation__link, #close-hamburger").on(
+    "click",
+    function () {
+      $(".navigation").slideToggle(500);
+      $(".hamburger-menu").toggleClass("hamburger-menu--open");
+    }
+  );
 });
